3cd7d4e46d71f3a28c81d80c18cd2fde0b16b0b3
alebusi <allebusi@gmail.com>
2021-12-14 11:23:22 +0100

    Update counter.js

--- counter.js
+++ counter.js
@@ -1,6 +1,9 @@
 var tess = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0];
 //var cambio = 0;
 var secondi = 0;
+var svg = '<svg viewBox="0 0 66 44">';
+var campo = '<path class="i1" d="M 0 0 h66 v44 h-66 z "/> <path class="i3" d="M9 6 h48 m-48 4 h48 m-48 4 h48 m-48 4 h48 m-48 4 h48 m-48 4 h48 m-48 4 h48 m-48 4 h48 m-48 4 h48 m-48 4" />';
+
 //var audioCampo = new Audio('campo.mp3');
 //var suona = new Audio('success.wav');
 //var audioTogli = new Audio('togli.mp3');
@@ -82,27 +85,28 @@ function myfunc2(div) {
   if (document.getElementById("controllo").className=="standard") { 
     if (className=="spazio") {
       div.className = "campo";
-      div.innerHTML = "";
+      div.innerHTML = svg+campo+'</svg>';
+      //div.innerHTML="";
       }
     else if (className=="campo") {
       div.className = "grano_3";      
-      div.innerHTML = '<svg viewBox="0 0 66 44"> <path class="i1" d="M 0 0 h66 v44 h-66 z "/> <path class="i2" d="M26 4 q9 0 9 6 t-9 6 t-9 -6 t9 -6 z m9 6 v4 q0 6 -9 6 t-9 -6 v-4m30 5 q9 0 9 6 t-9 6 t-9 -6 t9 -6 z m9 6 v4 q0 6 -9 6 t-9 -6 v-4m-21 4 q9 0 9 6 t-9 6 t-9 -6 t9 -6 z m9 6 v4 q0 6 -9 6 t-9 -6 v-4" /></svg>';
+      div.innerHTML = svg+campo+'<path class="i2" d="M26 4 q9 0 9 6 t-9 6 t-9 -6 t9 -6 z m9 6 v4 q0 6 -9 6 t-9 -6 v-4m30 5 q9 0 9 6 t-9 6 t-9 -6 t9 -6 z m9 6 v4 q0 6 -9 6 t-9 -6 v-4m-18 2 q9 0 9 6 t-9 6 t-9 -6 t9 -6 z m9 6 v4 q0 6 -9 6 t-9 -6 v-4" /></svg>';
       }
     else if (className=="grano_3") {
       div.className = "grano_2";
-      div.innerHTML = '<svg viewBox="0 0 66 44"> <path class="i1" d="M 0 0 h66 v44 h-66 z "/> <path class="i2" d="M26 4 q9 0 9 6 t-9 6 t-9 -6 t9 -6 z m9 6 v4 q0 6 -9 6 t-9 -6 v-4m30 5 q9 0 9 6 t-9 6 t-9 -6 t9 -6 z m9 6 v4 q0 6 -9 6 t-9 -6 v-4 " /></svg>';
+      div.innerHTML = svg+campo+'<path class="i2" d="M26 4 q9 0 9 6 t-9 6 t-9 -6 t9 -6 z m9 6 v4 q0 6 -9 6 t-9 -6 v-4m30 5 q9 0 9 6 t-9 6 t-9 -6 t9 -6 z m9 6 v4 q0 6 -9 6 t-9 -6 v-4 " /></svg>';
       }
     else if (className=="grano_2") {
       div.className = "grano_1";
-      div.innerHTML = '<svg viewBox="0 0 66 44"> <path class="i1" d="M 0 0 h66 v44 h-66 z "/> <path class="i2" d="M26 4 q9 0 9 6 t-9 6 t-9 -6 t9 -6 z m9 6 v4 q0 6 -9 6 t-9 -6 v-4" /></svg>';
+      div.innerHTML = svg+campo+'<path class="i2" d="M26 4 q9 0 9 6 t-9 6 t-9 -6 t9 -6 z m9 6 v4 q0 6 -9 6 t-9 -6 v-4" /></svg>';
       }
     else if (className=="grano_1") {
       div.className = "ortaggio_2";
-      div.innerHTML = '<svg viewBox="0 0 66 44"> <path class="i1" d="M 0 0 h66 v44 h-66 z "/> <path class="i2" d="M26 4 q9 0 9 6 t-9 6 t-9 -6 t9 -6 z m9 6 v4 q0 6 -9 6 t-9 -6 v-4m30 5 q9 0 9 6 t-9 6 t-9 -6 t9 -6 z m9 6 v4 q0 6 -9 6 t-9 -6 v-4 " /></svg>';
+      div.innerHTML = svg+campo+'<path class="i2" d="M26 4 q9 0 9 6 t-9 6 t-9 -6 t9 -6 z m9 6 v4 q0 6 -9 6 t-9 -6 v-4m30 5 q9 0 9 6 t-9 6 t-9 -6 t9 -6 z m9 6 v4 q0 6 -9 6 t-9 -6 v-4 " /></svg>';
       }
     else if (className=="ortaggio_2") {
       div.className = "ortaggio_1";
-      div.innerHTML = '<svg viewBox="0 0 66 44"> <path class="i1" d="M 0 0 h66 v44 h-66 z "/> <path class="i2" d="M26 4 q9 0 9 6 t-9 6 t-9 -6 t9 -6 z m9 6 v4 q0 6 -9 6 t-9 -6 v-4" /></svg>';
+      div.innerHTML = svg+campo+'<path class="i2" d="M26 4 q9 0 9 6 t-9 6 t-9 -6 t9 -6 z m9 6 v4 q0 6 -9 6 t-9 -6 v-4" /></svg>';
       }
     else if (className=="ortaggio_1") {
       div.className = "spazio";
@@ -110,13 +114,14 @@ function myfunc2(div) {
   }
   else if (document.getElementById("controllo").className=="edilizia") {
     if (className=="spazio") {
-      div.className = "stanza";
-      div.innerHTML = '<svg viewBox="0 0 66 44"> <path class="i1" d="M 0 0 h66 v44 h-66 z "/> <path class="i2" d="M9 6 h48 v32 h-48 z "/> <path class="i3" d="M9 6 h48 v8 h-48 z m24 16 m0 8 h16 v4 h-16 z m-10 -8 h6 v4 h-6 z m0 8 h6 v4 h-6 z"/> <path class="i2" d="M33 18 h16 v12 h-16 z m0 12 m-10 -8h6 v-4 h-6 z m0 8 h6 v-4 h-6 z " /></svg>';
-      }
-    else if (className=="stanza") {
       div.className = "stalla";
-      } 
+      div.innerHTML = svg+'<path class="i1" d="M 0 0 M18 20 v8 l12 9 v-8 l-6 -10 zl6 -1 l18 -12 l-6 1 zm12 9 v8 l18 -12 v-8 zl-6 -10 l18 -12 l6 10 z" /></svg>';
+      }
     else if (className=="stalla") {
+      div.className = "stanza";
+      div.innerHTML = svg+'<path class="i1" d="M 0 0 h66 v44 h-66 z "/> <path class="i2" d="M9 6 h48 v32 h-48 z "/> <path class="i3" d="M9 6 h48 v8 h-48 z m24 16 m0 8 h16 v4 h-16 z m-10 -8 h6 v4 h-6 z m0 8 h6 v4 h-6 z"/> <path class="i4" d="M33 18 h16 v12 h-16 z m0 12 m-10 -8h6 v-4 h-6 z m0 8 h6 v-4 h-6 z " /></svg>';      
+      } 
+    else if (className=="stanza") {
       div.className = "stanza_argilla";
       }
     else if (className=="stanza_argilla") {
