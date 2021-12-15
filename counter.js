35b0c3c61344eecd433350086d751c68674f3106
alebusi <allebusi@gmail.com>
2021-12-15 17:36:01 +0100

    Update counter.js

--- counter.js
+++ counter.js
@@ -13,39 +13,26 @@ document.getElementById("stanza2").innerHTML=stanza;
 //var audioTogli = new Audio('togli.mp3');
 
 function myfunc(id,i,ind) {
-  if (document.getElementById("buttonRisorsa").className == "standard")
+  if (document.getElementById("controllo").className == "standard")
     {
     document.getElementById(id).className = "trasparente";
     tess[ind]=0;
     document.getElementById(id).innerHTML = tess[ind];
     //audioTogli.play();
     }
-  else if (document.getElementById("buttonRisorsa").className == "steccati")
+  else if (document.getElementById("controllo").className == "edilizia")
     {
     document.getElementById(id).className = "risorsa";
     tess[ind]+=1;
     document.getElementById(id).innerHTML = tess[ind];
     //suona.play();
     }
-  else if (document.getElementById("buttonRisorsa").className == "edilizia")
+  else if (document.getElementById("controllo").className == "steccati")
     {
     document.getElementById(id).className = "risorsa";
     tess[ind]-=1;
     document.getElementById(id).innerHTML = tess[ind];
     //suona.play();
-    }
-}
-
-function btAggRisorsa(div) {
-  var className = div.getAttribute("class");
-  if (className=="zero") {
-    div.className = "somma";
-    }
-  else if (className=="somma") {
-    div.className = "sottrai";
-    }
-  else if (className=="sottrai") {
-    div.className = "zero";
     }
 }
 
